3aa11195dc22a1a7417f847e61bd34c1d88b2216
AndyChen <shinningstar1001@gmail.com>
2018-01-08 23:46:03 +0800

    first commit

---
+++ blockchain.js
@@ -0,0 +1,42 @@
+// Create blockchain
+const crypto = require('crypto')
+
+const blockChains = []
+function calcHash(data, preHash) {
+    return crypto.createHmac('sha256', 'my secret')
+        .update(data)
+        .digest('hex');
+}
+
+function generateBlock({hash: preHash}, data) {
+    return {
+        data,
+        hash: calcHash(data + preHash),
+        preHash
+    }
+}
+
+function verifyBlock(preBlock, block) {
+    if (preBlock.hash !== block.preHash) {
+        return false
+    }
+
+    if (calcHash(preBlock.hash + block.data) !== block.hash) {
+        return false
+    }
+
+    return true
+}
+
+const firstBlock = generateBlock({ hash: 0 }, '我想藉由區塊練儲存的不可竄改資料')
+blockChains.push(firstBlock)
+blockChains.push(generateBlock(firstBlock, "新增的第二筆資料"))
+
+let lastBlock = blockChains[blockChains.length - 1]
+blockChains.push(generateBlock(lastBlock, "新增的資料"))
+//Create new block and save your data like this....
+//Use p2p module to sync blockchain
+//blockchain slow because each node is creating block, it will conflict. 
+//Resolve conflict called consensus, like voting 
+
+
